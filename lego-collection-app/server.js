d4f7b84480deaf49307d3e0cc0b9a6dff4652ef5
Saurab Khadka <150497452+skhadkarsk23@users.noreply.github.com>
2024-10-12 01:58:58 +0000

    Initial commit for Assignment 3

---
+++ lego-collection-app/server.js
@@ -0,0 +1,36 @@
+/********************************************************************************
+* WEB322 – Assignment 03
+*
+* I declare that this assignment is my own work in accordance with Seneca's
+* Academic Integrity Policy:
+*
+* https://www.senecacollege.ca/about/policies/academic-integrity-policy.html
+*
+* Name: Saurab Khadka Student ID: 148501224 Date: 2024/10/11
+*
+* Published URL: ___________________________________________________________
+*
+********************************************************************************/
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views/home.html'));
+});
+
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views/about.html'));
+});
+
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, 'views/404.html'));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
